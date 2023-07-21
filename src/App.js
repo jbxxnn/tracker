594eaf702979a9b5dadbcbb8762b3c0cebfe19e8
jbxxnn <jbxxnn6@gmail.com>
2023-07-21 14:18:34 +0100

    Friday Updates

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import logo from "./logo.svg";
 import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const Expenses = [
+    {
+      title: "Monday",
+      price: "357",
+      description: "This is monday",
+    },
+    {
+      title: "Tuesday",
+      price: "192",
+      description: "This is tuesday",
+    },
+    {
+      title: "Wednesday",
+      price: "920",
+      description: "This is wednesday",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+    <div>
+      <ExpenseItem
+        title={Expenses[0].title}
+        price={Expenses[0].price}
+        description={Expenses[0].description}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={Expenses[1].title}
+        price={Expenses[1].price}
+        description={Expenses[1].description}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={Expenses[2].title}
+        price={Expenses[2].price}
+        description={Expenses[2].description}
+      ></ExpenseItem>
     </div>
   );
 }
